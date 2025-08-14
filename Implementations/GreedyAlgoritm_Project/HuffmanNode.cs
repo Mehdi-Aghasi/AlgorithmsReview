969ebee0b8436b867f810260d57659645cac2890
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-08-14 20:53:31 +0330

    create huffmanNode class

---
+++ Implementations/GreedyAlgoritm_Project/HuffmanNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreedyAlgoritm_Project
+{
+    public class HuffmanNode
+    {
+        public char Symbol;
+        public int Frequency;
+        public HuffmanNode Left;
+        public HuffmanNode Right;
+    }
+}
